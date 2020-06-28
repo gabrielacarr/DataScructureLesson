85a5909758f58c7721d4c2ffbbbb5e42daae54bf
La Brish <b@Bs-MacBook-Pro.local>
2020-06-28 19:57:12 -0400

    reverseint

--- Exercises/reverseinteger.js
+++ Exercises/reverseinteger.js
@@ -1,1 +1,14 @@
 // --- Directions 
+// Given an integer, return an integer that is reverse
+// ordering of numbers.
+// --- Examples
+//      reversedInt(15) === 51
+//      reversedInt(981) === 189
+//      reversedInt(500) === 5
+//      reversedInt(-15) === -51
+//      reversedInt(-90) === -9
+
+
+function reverseInt(n) {
+    
+}
