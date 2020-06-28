c72c4e27ee2f2b57fb7542a96625ad6b7988d5ac
La Brish <b@Bs-MacBook-Pro.local>
2020-06-28 19:46:13 -0400

    revstr

--- Exercises/palindromes.js
+++ Exercises/palindromes.js
@@ -8,12 +8,12 @@
 // palidrome('abcdefg') === false
 
 ///// first attempt
-function palidrome(str) {
+function palindrome(str) {
     return pali.split('').reverse((rev, char) => char + rev ? true : false)
 }
 
 // first solution
-function palidrome(str) {
+function palindrome(str) {
   const reversed = str.split('').reverse().join('');
 
   return str === reversed;
@@ -24,9 +24,77 @@ function palidrome(str) {
 // bollean check on every element within an array
 // ex. array.every((val) => val > 5);
 // checks twice and first solution should be used  
-function palidrome(str) {
+function palindrome(str) {
    return str.split('').every((char, i) => {
         return char === str[str.length - i - 1];
     })
 }
 
+// learning
+function palindrome(str) {
+  const reversed = str.split('').reverse().join('');
+
+return str === reversed;
+}
+
+// .every() returns a boolean
+// boolean check on every element in the array
+
+function palindrome(str) {
+ return str.split('').every((char, i) => {
+return char === str[str.length - i - 1];
+  })
+}
+
+
+function palindrom(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1]
+  });
+}
+
+function palindrome(str) {
+  const reversed = str.split('').reverse().join('');
+  return str === reversed;
+}
+
+
+
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1]
+  });
+}
+
+function palindrome(str) {
+  const reversed = str.split('').reversed().join('');
+  return str === reversed;
+}
+
+function palindrome(str) {
+  return str.split('').every((e, i) => {
+    return e  === str[str.length - i - 1];
+  })
+}
+
+function palidrome(str) {
+  const reversed = str.split('').reverse().join('');
+  return str === reversed;
+}
+
+/// if case sensitive use .toLowerCase()
+
+function palindrome(str) {
+  str = str.toLowerCase()
+  const resevered = str.split('').reversed().join('');
+  return str === resevered;
+}
+
+// time complexity
+// although methods are form of for loops
+// .join('') uses linear time complexity
+// O(N) time complexity  
+// it has to iterate through all the elements within the input string
+
+// space complexity
+// O(N) is the same length of the input string 
