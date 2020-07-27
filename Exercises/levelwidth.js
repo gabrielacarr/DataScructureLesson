51fb9312635fbbd610f9fab7cf5088b0874927fb
Brie <briecardo@icloud.com>
2020-07-27 09:19:07 -0700

    Solution for level width

--- Exercises/levelwidth.js
+++ Exercises/levelwidth.js
@@ -1,4 +1,5 @@
 // --- Directions
+// Level Width 
 // Given the root node of a tree, return
 // an array where each element is the width
 // of the tree at each level.
@@ -10,3 +11,22 @@
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
+
+function levelWidth(root) {
+    const arr = [root, 's'];
+    const counters = [0];
+  
+    while (arr.length > 1) {
+      const node = arr.shift();
+  
+      if (node === 's') {
+        counters.push(0);
+        arr.push('s');
+      } else {
+        arr.push(...node.children);
+        counters[counters.length - 1]++;
+      }
+    }
+  
+    return counters;
+  }
