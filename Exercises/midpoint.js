e433025a31aa0bcfdb1d4482fd4a6b7d9036ac50
Brie <briecardo@icloud.com>
2020-08-12 08:45:56 -0700

    Midpoint solution provided

--- Exercises/midpoint.js
+++ Exercises/midpoint.js
@@ -11,3 +11,15 @@
 //   l.insertLast('b')
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
+
+function midpoint(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+  
+    while (fast.next && fast.next.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+  
+    return slow;
+  }
