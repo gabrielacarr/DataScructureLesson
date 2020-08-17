981d8f15cb1eeed5f8d6b2c08424f67372a3b4c1
Brie <briecardo@icloud.com>
2020-08-17 16:49:23 -0700

    Implementation of selectionSort for the solution

--- sorting.js
+++ sorting.js
@@ -13,7 +13,29 @@ function bubbleSort(arr) {
         }
       }
     }
-    
+
   // return the sorted array
   return arr;
 }
+
+function selectionSort(arr) {
+    // Implement selectionSort
+    for (let i = 0; i < arr.length; i++) {
+      let indexOfMin = i;
+  
+      for (let j = i+1; j <arr.length; j++) {
+        if (arr[j] < arr[indexOfMin]) {
+          indexOfMin = j;
+        }
+      }
+  
+      if (indexOfMin !== i) {
+        let lesser = arr[indexOfMin];
+        arr[indexOfMin] = arr[i];
+        arr[i] = lesser;
+      }
+    }
+  
+    return arr;
+  }
+  
