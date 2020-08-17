d5a931ea90172a0d28d406e17b9df5915fbf385f
Brie <briecardo@icloud.com>
2020-08-17 16:50:11 -0700

    Implementation of mergeSort into the solution

--- sorting.js
+++ sorting.js
@@ -39,3 +39,16 @@ function selectionSort(arr) {
     return arr;
   }
   
+  function mergeSort(arr) {
+      // Implementtion of mergeSort
+    if (arr.length === 1) {
+      return arr;
+    }
+  
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0, center);
+    const right = arr.slice(center);
+  
+    return merge(mergeSort(left), mergeSort(right));
+  }
+  
