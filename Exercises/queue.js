25323502715f19906c5dad2da942040614d447f8
Brie <briecardo@icloud.com>
2020-09-07 16:08:57 -0700

    Solution

--- Exercises/queue.js
+++ Exercises/queue.js
@@ -7,3 +7,18 @@
 //     const q = new Queue();
 //     q.add(1);
 //     q.remove(); // returns 1;
+
+class Queue {
+    constructor() {
+      this.data = [];
+    }
+  
+    add(record) {
+      this.data.unshift(record);
+    }
+  
+    remove() {
+      return this.data.pop();
+    }
+  }
+  
