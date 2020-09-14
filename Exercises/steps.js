3fdda9cf464a96ab7839d96bd328e73b753aa5dc
Brie <briecardo@icloud.com>
2020-09-14 10:17:27 -0700

    Solution Two for steps

--- Exercises/steps.js
+++ Exercises/steps.js
@@ -30,4 +30,19 @@ function steps(n, row = 0, stair = '') {
     const add = stair.length <= row ? '#' : ' ';
     steps(n, row, stair + add);
   }
-  
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+//
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//
+//     console.log(stair);
+//   }
+// }
