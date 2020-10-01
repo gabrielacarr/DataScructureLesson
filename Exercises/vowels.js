16313d30fae6f1ff3f9c9d84e459626b99d543e6
Brie <briecardo@icloud.com>
2020-10-01 09:51:19 -0700

    Solution two

--- Exercises/vowels.js
+++ Exercises/vowels.js
@@ -11,4 +11,17 @@ function vowels(str) {
     const matches = str.match(/[aeiou]/gi);
     return matches ? matches.length : 0;
   }
-  
+
+
+  // function vowels(str) {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+//
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++;
+//     }
+//   }
+//
+//   return count;
+// }
